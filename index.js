7b63128535c68a1e26fe2fa89b3b748277b4b51f
krouw <felipe.andrez@live.cl>
2017-11-21 00:05:20 -0300

    To RAsp

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const Meter = require('./src/meter')
 const config = {
   id: '01',
   xbeeProductId: '6001',
-  mqtt: 'mqtt://localhost:1883'
+  mqtt: 'mqtt://192.168.0.2:1883'
 }
 
 const meter = new Meter( config.id ,config.xbeeProductId, config.mqtt )
