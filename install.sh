923f9218f708b962f1bafed900d81b7566060b7c
krouw <felipe.andrez@live.cl>
2019-03-15 00:11:57 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
-npm install
+npm install && node-gyp configure build
+pm2 startup systemd -u pi
+sudo env PATH=$PATH:/usr/bin pm2 startup systemd -u pi --hp /home/pi
 pm2 start index.js --name smart-gateway -e err.log -o out.log
 pm2 save
