95cfd160a674685931d4807570bdc5b7af1cf34d
krouw <felipe.andrez@live.cl>
2019-05-10 17:53:16 -0400

    update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
-npm install && node-gyp configure build
+
+if ! service --status-all | grep -Fq "pm2"; then
+   sudo npm install -g pm2
+fi
+
+npm install
 pm2 startup systemd -u pi
 sudo env PATH=$PATH:/usr/bin pm2 startup systemd -u pi --hp /home/pi
 pm2 start index.js --name smart-gateway -e err.log -o out.log
